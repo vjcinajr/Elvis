5c76f021ec5e018bba2d781c829bbea55df2031b
Vincent Cina <vcina@me.com>
2025-02-08 16:08:45 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by VINCENT CINA on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
